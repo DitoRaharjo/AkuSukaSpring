1fef9018f5cf37eb0ce4dc2ab973d4c13daa456b
Dito Raharjo <ditoraharjo@gmail.com>
2017-06-04 17:58:10 +0700

    Initialize Spring Boot Project

---
+++ src/main/java/com/akusukaspring/AkusukaspringApplication.java
@@ -0,0 +1,12 @@
+package com.akusukaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkusukaspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkusukaspringApplication.class, args);
+	}
+}
---
+++ src/test/java/com/akusukaspring/AkusukaspringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.akusukaspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AkusukaspringApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
